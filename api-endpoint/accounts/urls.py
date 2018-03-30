d38b40368190c0c965ae7292af33d2cc517c1da6
Nijinsha <mail.nijinsha@gmail.com>
2018-03-30 14:31:25 +0530

    adds login view

--- api-endpoint/accounts/urls.py
+++ api-endpoint/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import CreateUserView,vrthe
+from rest_framework_jwt.views import obtain_jwt_token
 
 
 urlpatterns=[
-    path('adduser/',CreateUserView.as_view(),name="adduser"),
-    path('changepassword/',CreateUserView.as_view(),name="adduser"),
+    path('adduser/',CreateUserView.as_view(), name="adduser"),
+    path('changepassword/',CreateUserView.as_view(), name="adduser"),
+    path('login/',obtain_jwt_token, name="login"),
     path('info/',vrthe)
 ]
