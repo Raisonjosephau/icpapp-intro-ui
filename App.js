3ea4a4a46309d7cca9cd5c4d7e5c878de50f4ed9
Raisonjosephau <raisonjosephau@gmail.com>
2018-03-30 10:10:18 +0530

    NativeBase

--- App.js
+++ App.js
@@ -1,27 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
 import React, { Component } from 'react';
-import Router from './Tabnav/NavTab';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-import { COLOR, ThemeProvider } from 'react-native-material-ui';
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
-// you can set your style right here, it'll be propagated to application
-const uiTheme = {
-    palette: {
-        primaryColor: COLOR.green800,
-    },
-    toolbar: {
-        container: {
-            height: 50,
-        },
-    },
-};
-
-
-export default class App extends Component {
-    render() {
-        return ( 
-            <ThemeProvider uiTheme = { uiTheme } >
-                <Router/>
-            </ThemeProvider>
-        );
-    }
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
